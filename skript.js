e4363ae33cc45dfd9d9baea1d5d504f5690457f4
buenni86 <12584248+buenni86@users.noreply.github.com>
2022-10-11 08:53:57 +0200

    Testing Büro

--- skript.js
+++ skript.js
@@ -1,12 +1,16 @@
+import { bootstrapExtra } from "@workadventure/scripting-api-extra"
+console.log("Script started successfully")
+// end: mox scripting
+
 import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
 
 var currentPopup = undefined;
 var isCoWebSiteOpened =  false;
+var urlTutorial = "https://db-planet.deutschebahn.com/pages/telefonie/apps/content/workadventure-erste-schritte";
+//var urlFeedback = "mailto:DB.Systel.CommunicationServices.EVS@deutschebahn.com";
 var urlFeedback = "https://forms.office.com/Pages/ResponsePage.aspx?id=nC2noeZJbU-a9lqvoRg7_f26WHDvlOFNi_8Y43fECOdUMDVDTUpUUDRONkxHMzdLQ09WRlQxUUZSMS4u";
-var urlInfo = "https://db-planet.deutschebahn.com/pages/telefonie/apps/content/workadventure";
-var urlMusik = "https://www.youtube-nocookie.com/embed/36YnV9STBqc?autoplay=1";
-var urlCafe = "https://www.chefkoch.de/rezepte/1092131215242366/Eiskaffee-Latte-macchiato.html";
-var Mail = "mailto:DB.Systel.CommunicationServices.EVS@deutschebahn.com";
+var urlInfo = "https://db.de/workadventure";
+var urlInfoOrder = "https://dbserviceportal.service-now.com/serviceportal?id=sc_cat_item&category_sys_id=undefined&sys_id=0fa1b33e1b4bf010159842229b4bcb0e";
 
 function closePopUp(){
     if (currentPopup !== undefined) {
@@ -16,26 +20,30 @@ function closePopUp(){
 }
 
 var zoneFeedback = "feedback";
+var zoneTutorial = "tutorial";
 var zoneInfo = "info";
-var zoneInfoDoor = "infoDoor";
-var zoneMusik = "musik";
-var zoneCafe = "cafe";
-var zoneTestURL = "testURL";
+var zoneInfoOrder = "info-order";
+var zoneTableTennis = "tabletennis";
+var zoneInformation = "information";
 
+var pongMsg = "Pong gegeneinander?\n\n1.Wählen Sie Online-Mehrspielermodus\n" +
+"2.Wählen Sie 'Beiläufig'\n3.Geben Sie eine Zimmernummer ein und klicken Sie auf 'Zimmer ändern'\n" +
+"4. Teilen Sie die Zimmernummer Ihrem Partner mit\n\n" +
+"Die Steuerung funktioniert mit den Pfeiltasten."
 
-WA.room.onEnterZone(zoneTestURL, () => {
-   currentPopup =  WA.ui.openPopup("popUpTestURL","Test URL öffnen?.",[
+WA.room.onEnterZone(zoneTutorial, () => {
+   currentPopup =  WA.ui.openPopup("popUpTutorial","Tutorial ansehen?",[
         {
             label: "OK",
             callback: (popup => {
-                WA.nav.openCoWebSite(WA.state.testURL, false, "autoplay;camera;microphone;fullscreen;encrypted-media");
+                WA.nav.openTab(urlTutorial);
                 isCoWebSiteOpened = true;
                 closePopUp();
             })
         }]);
 })
 
-WA.room.onLeaveZone(zoneTestURL, () =>{
+WA.room.onLeaveZone(zoneTutorial, () =>{
     closePopUp();
 
     if (isCoWebSiteOpened) {
@@ -47,9 +55,9 @@ WA.room.onLeaveZone(zoneTestURL, () =>{
 WA.room.onEnterZone(zoneFeedback, () => {
    currentPopup =  WA.ui.openPopup("popUpFeedback","Hier kannst du Feedback abgeben.",[
         {
-            label: "OK",
+            label: "Feedback",
             callback: (popup => {
-                WA.nav.openCoWebSite(WA.state.feedbackURL, false, "autoplay; fullscreen; encrypted-media");
+                WA.nav.openCoWebSite(urlFeedback);
                 isCoWebSiteOpened = true;
                 closePopUp();
             })
@@ -66,10 +74,10 @@ WA.room.onLeaveZone(zoneFeedback, () =>{
 })
 
 WA.room.onEnterZone(zoneInfo, () => {
-   currentPopup =  WA.ui.openPopup("popUpInfo","Willkommen im öffentlichen Besprächungsraum Team EVS, möchtest du mehr Informationen zu WorkAdventure erfahren?",[
+   currentPopup =  WA.ui.openPopup("popUpInfo","Willkommen im EVS-Teambüro! Sprich uns gerne direkt an und komm bei uns vorbei! \n Mehr zu Workadventure auf ...",[
         {
-            label: "Her damit!",
-			callback: (popup => {
+            label: "DB Planet",
+            callback: (popup => {
                 WA.nav.openTab(urlInfo);
                 isCoWebSiteOpened = true;
                 closePopUp();
@@ -86,58 +94,20 @@ WA.room.onLeaveZone(zoneInfo, () =>{
     }
 })
 
-WA.room.onEnterZone(zoneInfoDoor, () => {
-   currentPopup =  WA.ui.openPopup("popUpInfoDoor","Falls die Tür verschlossen ist, am besten einen Termin mit uns vereinbaren\n📧 Team EVS 📧",[
+
+WA.room.onEnterZone(zoneInfoOrder, () => {
+   currentPopup =  WA.ui.openPopup("popUpInfoOrder","Ihr möchtet auch ein eigenes Teambüro mit kurzer Info zu euch? Hier könnt ihr dies beantragen.",[
         {
-            label: "E-Mail",
-			className:"error",
-			callback: (popup => {
-                WA.nav.openTab(Mail);
-                isCoWebSiteOpened = true;
-                closePopUp();
-            })
-        }]);
-})
-
-WA.room.onLeaveZone(zoneInfoDoor, () =>{
-    closePopUp();
-})
-
-
-WA.room.onEnterZone(zoneMusik, () => {
-   currentPopup =  WA.ui.openPopup("popUpMusik","Ein wenig Musik gefällig?",[
-        {
-            label: "Her damit!",
-			callback: (popup => {
-                WA.nav.openCoWebSite(urlMusik, false, "autoplay; fullscreen; encrypted-media");
-                isCoWebSiteOpened = true;
-                closePopUp();
-            })
-        }]);
-})
-
-WA.room.onLeaveZone(zoneMusik, () =>{
-    closePopUp();
-
-    if (isCoWebSiteOpened) {
-        WA.nav.closeCoWebSite();
-        isCoWebSiteOpened = false;
-    }
-})
-
-WA.room.onEnterZone(zoneCafe, () => {
-   currentPopup =  WA.ui.openPopup("popUpCafe","Leider,\ndo it yourself",[
-        {
-            label: "Empfehlung",
+            label: "Digitalportal",
             callback: (popup => {
-                WA.nav.openTab(urlCafe);
+                WA.nav.openTab(urlInfoOrder);
                 isCoWebSiteOpened = true;
                 closePopUp();
             })
         }]);
 })
 
-WA.room.onLeaveZone(zoneCafe, () =>{
+WA.room.onLeaveZone(zoneInfoOrder, () =>{
     closePopUp();
 
     if (isCoWebSiteOpened) {
@@ -145,3 +115,134 @@ WA.room.onLeaveZone(zoneCafe, () =>{
         isCoWebSiteOpened = false;
     }
 })
+
+
+WA.room.onEnterZone(zoneTableTennis, () => {
+   currentPopup =  WA.ui.openPopup("popUpTableTennis", pongMsg, [
+        {
+            label: "Verstanden",
+            callback: (popup => {
+                isCoWebSiteOpened = true;
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onLeaveZone(zoneTableTennis, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
+
+WA.room.onEnterZone(zoneInformation, () => {
+   currentPopup =  WA.ui.openPopup("popUpInformation","Der Sommer ist da, schnapp dir ein Eis und besuche uns draußen vor dem Silberturm!\nLauf einfach in den Foyer und spring ins Portal in die DB Workadventure WorldTour!",[
+        {
+            label: "OK",
+            callback: (popup => {
+                isCoWebSiteOpened = true;
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onLeaveZone(zoneInformation, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
+
+// start: mox scripting
+const buttons = [
+    {
+      label: "Reset",
+      className: "error",
+      callback: () =>
+        (WA.state.votePos = WA.state.voteNeg = WA.state.voteNeut = 0)
+    }
+  ]
+  
+  // Waiting for the API to be ready
+  WA.onInit()
+    .then(() => {
+      console.log("Scripting API ready")
+      console.log("Player tags: ", WA.player.tags)
+  
+      let website
+      WA.room.onEnterLayer("infoPopup").subscribe(() => {
+        openInfoWebsite().then(_website => (website = _website))
+      })
+  
+      WA.room.onLeaveLayer("infoPopup").subscribe(() => {
+        website.visible = false
+      })
+  
+      WA.room.onEnterLayer("votePos").subscribe(() => {
+        console.log("VotePos: ", WA.state.votePos)
+        WA.state.votePos++
+      })
+      WA.room.onLeaveLayer("votePos").subscribe(() => {
+        console.log("VotePos: ", WA.state.votePos)
+        if (WA.state.votePos === 0) return
+        WA.state.votePos--
+      })
+      WA.room.onEnterLayer("voteNeg").subscribe(() => {
+        console.log("voteNeg: ", WA.state.voteNeg)
+        WA.state.voteNeg++
+      })
+      WA.room.onLeaveLayer("voteNeg").subscribe(() => {
+        console.log("voteNeg: ", WA.state.voteNeg)
+        if (WA.state.voteNeg === 0) return
+        WA.state.voteNeg--
+      })
+      WA.room.onEnterLayer("voteNeut").subscribe(() => {
+        console.log("voteNeut: ", WA.state.voteNeut)
+        WA.state.voteNeut++
+      })
+      WA.room.onLeaveLayer("voteNeut").subscribe(() => {
+        console.log("voteNeut: ", WA.state.voteNeut)
+        if (WA.state.voteNeut === 0) return
+        WA.state.voteNeut--
+      })
+  
+      let voteResetPopup
+      WA.room.onEnterLayer("voteRes").subscribe(() => {
+        voteResetPopup = WA.ui.openPopup(
+          "resetPopup",
+          "Soll das Voting zurückgesetzt werden?",
+          buttons
+        )
+      })
+      WA.room.onLeaveLayer("voteRes").subscribe(() => {
+        voteResetPopup.close()
+      })
+  
+      // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+      bootstrapExtra()
+        .then(() => {
+          console.log("Scripting API Extra ready")
+        })
+        .catch(e => console.error(e))
+    })
+    .catch(e => console.error(e))
+  
+  async function openInfoWebsite() {
+    return await WA.ui.website.open({
+      url: "https://tstosius.github.io/worktest/info.html",
+      position: {
+        vertical: "middle",
+        horizontal: "middle"
+      },
+      size: {
+        width: "50vh",
+        height: "50vh"
+      }
+    })
+  }
+  
+  export {}
